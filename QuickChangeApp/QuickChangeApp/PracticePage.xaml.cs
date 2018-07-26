7c3c5987cc6727ceade23e1f72e202d267145bbd
Kevin Sanders <nolnah93@outlook.com>
2018-07-26 17:17:14 -0400

    Updated the PracticePage.xaml layout to work better on the default emulator device and my own device.

--- QuickChangeApp/QuickChangeApp/PracticePage.xaml.cs
+++ QuickChangeApp/QuickChangeApp/PracticePage.xaml.cs
@@ -15,30 +15,79 @@ namespace QuickChangeApp
         int score { get; set; }
         decimal amountOwed { get; set; }
         decimal amountPaying { get; set; }
+        Mode currentMode { get; set; }
 		public PracticePage ()
 		{
 			InitializeComponent ();
 
-            Random r = new Random();
             score = 0;
-            amountOwed = Math.Round((Decimal)r.NextDouble() * 100, 2);
-            amountPaying = Math.Round((Decimal)r.NextDouble() * 100 + amountOwed, 2);
-            QuestionLabel.Text = $"You owe ${amountOwed}.You have ${amountPaying}. How much change will you get back?";
+            currentMode = Mode.EASY;
+            DetermineDifficultyAndCalculateQuestion();
 
         }
-
+        private void DetermineDifficultyAndCalculateQuestion()
+        {
+            if (currentMode == Mode.EASY)
+            {
+                CalculateQuestion(1);
+            }
+            else if (currentMode == Mode.MEDIUM)
+            {
+                CalculateQuestion(10);
+            }
+            else if (currentMode == Mode.HARD)
+            {
+                CalculateQuestion(100);
+            }
+        }
+        private void CalculateQuestion(int multiplier)
+        {
+            Random r = new Random();
+            amountOwed = Math.Round((Decimal)r.NextDouble() * multiplier, 2);
+            amountPaying = Math.Round((Decimal)r.NextDouble() * multiplier + amountOwed, 2);
+            QuestionLabel.Text = $"You owe ${amountOwed}.You have ${amountPaying}. How much change will you get back?";
+            AnswerEntry.Text = "";
+        }
         private void SubmitButton_Clicked(object sender, EventArgs e)
         {
             if (Convert.ToDecimal(AnswerEntry.Text) == amountPaying - amountOwed)
             {
                 ScoreLabel.Text = $"Score: {++score}";
 
-                Random r = new Random();
-                amountOwed = Math.Round((Decimal)r.NextDouble() * 100, 2);
-                amountPaying = Math.Round((Decimal)r.NextDouble() * 100 + amountOwed, 2);
-                QuestionLabel.Text = $"You owe ${amountOwed}.You have ${amountPaying}. How much change will you get back?";
+                DetermineDifficultyAndCalculateQuestion();
                 AnswerEntry.Text = "";
+                ResultLabel.Text = "Correct!!!!!";
+                ResultLabel.TextColor = Color.Green;
+            }
+            else
+            {
+                ResultLabel.Text = "Sorry! That is incorrect";
+                ResultLabel.TextColor = Color.Red;
             }
         }
+
+        private void EasyButton_Clicked(object sender, EventArgs e)
+        {
+            currentMode = Mode.EASY;
+            DetermineDifficultyAndCalculateQuestion();
+        }
+
+        private void MediumButton_Clicked(object sender, EventArgs e)
+        {
+            currentMode = Mode.MEDIUM;
+            DetermineDifficultyAndCalculateQuestion();
+        }
+
+        private void HardButton_Clicked(object sender, EventArgs e)
+        {
+            currentMode = Mode.HARD;
+            DetermineDifficultyAndCalculateQuestion();
+        }
+    }
+    public enum Mode
+    {
+        EASY,
+        MEDIUM,
+        HARD
     }
 }
